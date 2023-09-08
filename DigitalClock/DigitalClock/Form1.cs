b942ffaf41f49c4deeeceb24a4e632f998c116e3
PATOLIYA DRASHTI <109703070+drashtipatel2804@users.noreply.github.com>
2023-09-08 22:04:12 +0530

    Add files via upload

---
+++ DigitalClock/DigitalClock/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DigitalClock
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = DateTime.Now.ToString("hh");
+            label2.Text = DateTime.Now.ToString("mm");
+            label3.Text = DateTime.Now.ToString("ss");
+
+        }
+    }
+}
